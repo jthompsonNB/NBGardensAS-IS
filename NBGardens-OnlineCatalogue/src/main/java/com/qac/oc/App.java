08c3c5d20884982dd1d7f94b6b72d510189542ab
jthompsonNB <james.thompson@qa.com>
2016-11-29 14:45:57 +0000

    comminting App class

--- NBGardens-OnlineCatalogue/src/main/java/com/qac/oc/App.java
+++ NBGardens-OnlineCatalogue/src/main/java/com/qac/oc/App.java
@@ -3,5 +3,10 @@ package com.qac.oc;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * This class is used ins SOA to specify the root for web service uri's
+ * 
+ * @author James Thompson
+ */
 @ApplicationPath(value="/ocapi")
 public class App extends Application { }
